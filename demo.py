374f4d21d77834716d765f0d52f44dc1ebb67188
VoVanThanh <thanhvo618@gmail.com>
2022-02-21 15:35:13 +0700

    1st

---
+++ demo.py
@@ -0,0 +1,32 @@
+# x = "ocschos"
+# print(x)
+
+# print(9*'a')
+# print('{0} {1} {0}'.format('a', 'b'))
+
+# age = int(input())
+# print(age)
+
+# x = input("Nhap: ")
+# print(x)
+
+# age = int(input())
+# if age >= 60:
+#     print('Gia')
+# elif age >= 18:
+#     pass
+# else:
+#     print('non')
+
+# a = 10
+# b = 5
+# c = 113 if a != b else 115
+# print(c)
+
+# a = int(input())
+# result = a + a * 11 + a * 111 + a * 1111
+# print(result)
+
+data = [('Tom', 19, 80), ('John', 20, 90), ('Jony', 17, 91), ('Jony', 17, 93), ("Json", 21, 85)]
+data.sort(key=lambda row: (row[0], row[1], row[2]))
+print(data)
