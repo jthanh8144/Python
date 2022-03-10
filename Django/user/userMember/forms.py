1ee051963660e6c38ca7b5d2c362ab352cc383c0
VoVanThanh <thanhvo618@gmail.com>
2022-03-10 17:36:12 +0700

    register, login, logout, decorators, mixins

---
+++ Django/user/userMember/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+class registerForm(forms.Form):
+    username = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    password = forms.CharField(max_length=25, widget=forms.PasswordInput)
+
+class loginForm(forms.Form):
+    username = forms.CharField(max_length=25)
+    password = forms.CharField(max_length=25, widget=forms.PasswordInput)
