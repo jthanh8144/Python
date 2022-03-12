e9d8e5d22e9a5ca1f70243cd01b3428b1257eef1
VoVanThanh <thanhvo618@gmail.com>
2022-03-12 20:24:37 +0700

    login with both email and username

--- a.py
+++ a.py
@@ -1,9 +1,7 @@
 import io
 import socket
 import struct
-# from threading import Thread
 from PIL import Image
-# from numpy import imag
 from _thread import *
 
 def thread_client(connection):
@@ -24,13 +22,9 @@ def thread_client(connection):
             break
     connection.close()
 
-# thread_count = 0
 server_socket = socket.socket()
 server_socket.bind(('0.0.0.0', 9000))
 server_socket.listen(0)
-
-# Accept a single connection and make a file-like object out of it
-# connection = server_socket.accept()[0].makefile('rb')
 
 while True:
     connection = server_socket.accept()[0].makefile('rb')
