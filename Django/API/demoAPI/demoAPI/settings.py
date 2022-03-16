f71c8c9639519e86364038263b8fde10f90ca78a
VoVanThanh <thanhvo618@gmail.com>
2022-03-16 20:49:39 +0700

    config postgresql

--- Django/API/demoAPI/demoAPI/settings.py
+++ Django/API/demoAPI/demoAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +76,13 @@ WSGI_APPLICATION = 'demoAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+with open("configdb.json") as db_config:
+    db = json.load(db_config)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        **db
     }
 }
 
