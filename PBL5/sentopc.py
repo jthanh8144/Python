74371eedb963367169c17054374bdbfe40850a55
VoVanThanh <thanhvo618@gmail.com>
2022-03-23 21:57:42 +0700

    pbl5 init

--- PBL5/sentopc.py
+++ PBL5/sentopc.py
@@ -5,28 +5,22 @@ import time
 import picamera
 
 client_socket = socket.socket()
-client_socket.connect(('192.168.1.8', 9000))
+client_socket.connect(('192.168.1.6', 9000))
 
-# Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
 try:
     with picamera.PiCamera() as camera:
         camera.resolution = (1024, 768)
-        # Start a preview and let the camera warm up for 2 seconds
         camera.start_preview()
         time.sleep(2)
 
         start = time.time()
         stream = io.BytesIO()
         for foo in camera.capture_continuous(stream, 'jpeg'):
-            # Write the length of the capture to the stream and flush to
-            # ensure it actually gets sent
             connection.write(struct.pack('<L', stream.tell()))
             connection.flush()
-            # Rewind the stream and send the image data over the wire
             stream.seek(0)
             connection.write(stream.read())
-            # Reset the stream for the next capture
             stream.seek(0)
             stream.truncate()
             print('Done!')
